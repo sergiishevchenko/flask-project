e472b8ebb2b8a014ab313bd863e135908e351cd0
sergii <sergiishevchenko017@gmail.com>
2018-11-21 22:32:50 +0300

    first commit

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/mydatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    comment = db.Column(db.String(1000))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    result = Comments.query.all()
+    
+    return render_template('index.html', result=result)
 
 
 @app.route('/sign')
 def sign():
     return render_template('sign.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+
+    signature = Comments(name=name, comment=comment)
+    db.session.add(signature)
+    db.session.commit()
+
+    return render_template('index.html', name=name, comment=comment)
 
 
 @app.route('/home', methods=['GET', 'POST'])
